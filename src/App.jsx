bedf69b45088e914f7bf9e67365d806ab944b112
Petr Kolarik <kolarik.petr@outlook.com>
2023-03-17 22:15:28 +1100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Fragment, useState } from 'react'
+// import { GlobalStyles } from './components/styles/GlobalStyles'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Fragment>
+      <div className="App">
+        <h1>Budget App</h1>
+      </div>
+    </Fragment>
+  )
+}
+
+export default App
